fecb4597c810c4dc6f0ccd59b018bbd21542a055
Wesley Banfield <wesleybanfield@gmail.com>
2020-12-29 14:11:30 +0100

    Add login page

--- netcdf_editor_app/__init__.py
+++ netcdf_editor_app/__init__.py
@@ -29,4 +29,10 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World! from NetCDF Editor App'
 
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     return app
