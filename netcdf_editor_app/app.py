663ae033377beaa28f6bc237cfbb47b15bbafd5e
Wesley Banfield <wesleybanfield@gmail.com>
2020-12-30 11:16:11 +0100

    Add handling of what happens when file is clicked on home page + Steps + regrid

--- netcdf_editor_app/app.py
+++ netcdf_editor_app/app.py
@@ -1,11 +1,12 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for, current_app
+    Blueprint, flash, g, redirect, render_template, request, url_for, current_app, session
 )
 from werkzeug.exceptions import abort
 from werkzeug.utils import secure_filename
 
 import os
 import tempfile
+import functools
 
 from netcdf_editor_app.auth import login_required
 from netcdf_editor_app.db import get_db
@@ -14,20 +15,25 @@ bp = Blueprint('app', __name__)
 
 
 @bp.route('/')
+@login_required
 def index():
+    # Remove the datafile if we go back to datafile selection screen
+    session.pop('data_file_id', None)
     db = get_db()
     data_files = db.execute(
-        'SELECT created, filename, username'
+        'SELECT created, filename, username, owner_id, df.id'
         ' FROM data_files df JOIN user u ON df.owner_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
 
     return render_template('app/index.html', data_files=data_files)
 
+
 def allowed_file(filename):
     ALLOWED_EXTENSIONS = {'nc'}
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @bp.route('/upload', methods=('GET', 'POST'))
 @login_required
@@ -46,7 +52,8 @@ def upload():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             temp_name = next(tempfile._get_candidate_names()) + ".nc"
-            file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], temp_name))
+            file.save(os.path.join(
+                current_app.config['UPLOAD_FOLDER'], temp_name))
             db = get_db()
             db.execute(
                 'INSERT INTO data_files (owner_id, filename, filepath)'
@@ -65,3 +72,35 @@ def upload():
       <input type=submit value=Upload>
     </form>
     '''
+
+
+@bp.route('/steps')
+@login_required
+def steps():
+    data_file_id = request.args.get('id')
+    if data_file_id is None:
+        return redirect(url_for('index'))
+    session['data_file_id'] = data_file_id
+    
+    db = get_db()
+    data_file_name = db.execute(
+        'SELECT filename FROM data_files WHERE id = ?', (data_file_id, )
+    ).fetchone()['filename']
+    return render_template('app/steps.html', data_file_name = data_file_name )
+
+
+def data_file_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if 'data_file_id' not in session.keys() or session['data_file_id'] is None:
+            return redirect(url_for('index'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
+@bp.route('/regrid')
+@login_required
+def regrid():
+    return render_template('app/regrid.html')
