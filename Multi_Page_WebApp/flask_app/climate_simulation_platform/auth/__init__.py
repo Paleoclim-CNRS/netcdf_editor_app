a1113407a64092ad9e757877bce171aa60eb325e
Wesley Banfield <wesleybanfield@gmail.com>
2021-05-20 11:36:22 +0200

    add comments

--- Multi_Page_WebApp/flask_app/climate_simulation_platform/auth/__init__.py
+++ Multi_Page_WebApp/flask_app/climate_simulation_platform/auth/__init__.py
@@ -3,17 +3,7 @@ import os
 
 from flask import Blueprint, redirect, url_for, g
 
-# Load correct auth based of from enviroment varaible
-
-#TODO in csp.__init__ this is already added to the flask config
-# we should probably use the same everywhere
-auth_config = os.environ.get("AUTH", "basic").lower()
-if auth_config == "basic":
-    from .auth_default import DefaultAuth as obj
-elif auth_config == 'logged_in':
-    from .auth_logged_in import LoggedInAuth as obj
-
-# Set global namespace
+# Module wide variables
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 def login_required(view):
@@ -26,6 +16,17 @@ def login_required(view):
 
     return wrapped_view
 
+# Load correct auth based of from enviroment varaible
+
+#TODO in csp.__init__ this is already added to the flask config
+# we should probably use the same everywhere
+auth_config = os.environ.get("AUTH", "basic").lower()
+if auth_config == "basic":
+    from .auth_default import DefaultAuth as obj
+elif auth_config == 'logged_in':
+    from .auth_logged_in import LoggedInAuth as obj
+
+# Set global
 @bp.route("/register", methods=("GET", "POST"))
 def register():
     return obj.register()
